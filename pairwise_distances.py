e96c96fa4ef7d3fc44d2e142ff7186581f11b765
Ben Butt <64893740+benbutt@users.noreply.github.com>
2020-08-28 11:19:56 +0100

    Update pairwise_distances.py

--- pairwise_distances.py
+++ pairwise_distances.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
-### pairwise_distances.py ###
-### Ben Butt July 2020 by GNU GPLv3 ###
+### pairwise_distances ###
 
-## From a list of coordinates as latitudes/longitudes, calculate and write a pairwise distance list ##
+## From a list of coordinates as latitudes/longitudes, calculate and write a pairwise distance list (in metres) using the Haversine formula (https://en.wikipedia.org/wiki/Haversine_formula) ##
+
 ## Usage: ./pairwise_distances.py <samples.csv> ##
 ## Input format: Name,Latitude,Longitude as CSV with header line ##
+
+## Ben Butt July 2020 by GNU GPLv3 ##
+
 
 # Import packages
 import math
